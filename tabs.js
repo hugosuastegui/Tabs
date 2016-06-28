45abbec9b083f84842c1cb159cbff8690a4d664f
codeamac06 <codeamac06@gmail.com>
2016-06-28 12:28:09 -0500

    Tabs, addClass, removeClass, nth-child

---
+++ tabs.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+  $("#tab1").css("display","block");
+  // Escribe tu código aquí
+    $(".tabs [href='#tab1']").click(function(){ 
+      $("#tab1").css("display","block");
+      $("#tab2").css("display","none");
+      $("#tab3").css("display","none");
+      $("li:nth-child(1)").addClass("active");
+      $("li:nth-child(2)").removeClass("active");
+      $("li:nth-child(3)").removeClass("active");
+      });
+    $(".tabs [href='#tab2']").click(function(){ 
+      $("#tab2").css("display","block");
+      $("#tab1").css("display","none");
+      $("#tab3").css("display","none");
+      $("li:nth-child(2)").addClass("active");
+      $("li:nth-child(1)").removeClass("active");
+      $("li:nth-child(3)").removeClass("active");
+      });
+    $(".tabs [href='#tab3']").click(function(){ 
+      $("#tab3").css("display","block");
+      $("#tab2").css("display","none");
+      $("#tab1").css("display","none");
+      $("li:nth-child(3)").addClass("active");
+      $("li:nth-child(2)").removeClass("active");
+      $("li:nth-child(1)").removeClass("active");
+      });
+    
+    // $( ".tabs li").addClass(function(index){
+    //   return 
+    // }
+});
